4240654025b76501f307a894251e7f9ec756f469
nathalieutstumo <nathalie.utstumo@gmail.com>
2022-05-30 18:10:11 +0200

    update

--- config/server.js
+++ config/server.js
@@ -1,16 +1,9 @@
-// module.exports = ({ env }) => ({
-//   host: env('HOST', '0.0.0.0'),
-//   port: env.int('PORT', 1337),
-
-//   app: {
-//     keys: env.array('APP_KEYS'),
-//   },
-// });
-
 module.exports = ({ env }) => ({
-	url: env('APP_URL'),
-	proxy: true,
-	app: {
-		keys: env.array('APP_KEYS', ['YxvI1uffnI9ZDrzkbN5G2A', 't66LEGoPxFbM8IRk96'])
-	}
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
 });
+
