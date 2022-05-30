cd01d4372214449311f15be63a3c8b4754c09dcc
nathalieutstumo <nathalie.utstumo@gmail.com>
2022-05-30 18:00:49 +0200

    update

--- config/server.js
+++ config/server.js
@@ -11,6 +11,6 @@ module.exports = ({ env }) => ({
 	url: env('APP_URL'),
 	proxy: true,
 	app: {
-		keys: env.array('APP_KEYS', ['YxvI1uffnI9ZDrzkbN5G2A==', 't66L+EGoPxFbM8IRk96+Vw=='])
+		keys: env.array('APP_KEYS', ['YxvI1uffnI9ZDrzkbN5G2A', 't66LEGoPxFbM8IRk96'])
 	}
 });
