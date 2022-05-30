c261526b6dc8cd2758a148717c7d52e785243540
nathalieutstumo <nathalie.utstumo@gmail.com>
2022-05-30 17:32:56 +0200

    update

--- config/server.js
+++ config/server.js
@@ -8,9 +8,9 @@
 // });
 
 module.exports = ({ env }) => ({
-  url: env("project-exam2-api.herokuapp.com"),
-  proxy: true,
-  app: {
-    keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
-  },
+	url: env('APP_URL'),
+	proxy: true,
+	app: {
+		keys: env.array('APP_KEYS', ['afe7405d67f206b6', '63e90c0711630990'])
+	}
 });
